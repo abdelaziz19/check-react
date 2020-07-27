7b7a048bf588af305e128b3af427d3755c43829e
abdelaziz darragi <abdelazizdarragi00@gmail.com>
2020-07-27 16:27:21 +0200

    react

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import { Form} from "react-bootstrap";
+
+
+function App() {
+  return (
+
+<Form className="formtag">
+  <Form.Group controlId="formGroupName">
+    <label>Name</label>
+    <Form.Control size ="lg" type="text" placeholder="Your Name" />
+  </Form.Group>
+  <Form.Group controlId="formGroupemail">
+    <label>Email address</label>
+    <Form.Control size ="lg" type="email" placeholder="your E-mail" />
+  </Form.Group>
+  <Form.Group controlId="formGroupMessage">
+    <label>Message</label>
+    <Form.Control size ="lg" type="text" placeholder="Your message" />
+  </Form.Group>
+  <button>Validate</button>
+</Form>
+  );
+}
+
+export default App;
